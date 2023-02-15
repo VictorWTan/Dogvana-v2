307701d86dbc717884913c6192d76f2d326856b2
victortan <vtan8090@gmail.com>
2023-02-15 13:34:49 -0500

    Added dropdown to mobile and banner message to desktop

--- src/Components/SponsorBar/SponsorBar.jsx
+++ src/Components/SponsorBar/SponsorBar.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import './SponsorBar.scss'
+import React, { useState } from "react";
+import "./SponsorBar.scss";
 
+const bannerText = "about the collaboration between Petfinder and Purina";
 
 export default function SponsorBar() {
-  return (
-    <div className='sponsor-banner'>
-            <span>POWERED BY</span>
-            <img className="purina-logo" src={require('../../Assets/purina.png')} alt="Purina Logo" />
+  const [dropdown, setDropdown] = useState(false);
 
-    </div>
-  )
+  const handleDropdown = (e) => {
+    e.preventDefault();
+    setDropdown(!dropdown);
+  };
+
+  return (
+    <>
+      <div className="sponsor-banner">
+        <div className="logo">
+          <span className="logo-text">POWERED BY</span>
+          <img
+            className="purina-logo"
+            src={require("../../Assets/purina.png")}
+            alt="Purina Logo"
+          />
+        </div>
+        <div className="sponsor-banner-desktop">
+          <a href="https://www.petfinder.com/purina-cares-partnership/">
+            Learn more
+          </a>
+          <span>{bannerText}</span>
+        </div>
+        <svg
+          onClick={handleDropdown}
+          className={!dropdown ? "bannerHidden" : "bannerOpen"}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+        </svg>
+      </div>
+      {dropdown && (
+        <div className="sponsor-banner-dropdown-mobile">
+          <a href="https://www.petfinder.com/purina-cares-partnership/">
+            Learn more
+          </a>
+          <span>{bannerText}</span>
+        </div>
+      )}
+    </>
+  );
 }
